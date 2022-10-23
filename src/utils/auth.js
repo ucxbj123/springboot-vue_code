096bfe585f6bb67a4d996f12567b0d3b51be8c08
xie <xie@qq.com>
2022-10-24 01:14:35 +0800

    修改auth.js将token存在localStorage（本地）避免因浏览器关闭导致token丢失

--- src/utils/auth.js
+++ src/utils/auth.js
@@ -5,13 +5,19 @@ import Cookies from 'js-cookie'
 const TokenKey = 'USERTOKEN'
 
 export function getToken() {
-  return Cookies.get(TokenKey)
+  // return Cookies.get(TokenKey)
+  Cookies.get(TokenKey)
+  return localStorage.getItem(TokenKey)
 }
 
 export function setToken(token) {
-  return Cookies.set(TokenKey, token)
+  // return Cookies.set(TokenKey, token)
+  Cookies.set(TokenKey, token)
+  return localStorage.setItem(TokenKey,token)
 }
 
 export function removeToken() {
-  return Cookies.remove(TokenKey)
+  // return Cookies.remove(TokenKey)
+  Cookies.remove(TokenKey)
+  return localStorage.removeItem(TokenKey)
 }
