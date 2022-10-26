6931bbdc9a433c54cfe7899bdb6122cf07cfc781
xie <xie@qq.com>
2022-10-27 00:23:01 +0800

    UserLists.vue 新增操作模块

--- src/views/system/UserLists.vue
+++ src/views/system/UserLists.vue
@@ -15,12 +15,30 @@
       <el-col :span="2" :offset="1">
         <el-button type="primary" icon="el-icon-search" @click.native="SearchUser" :disabled="buttonDisabled">搜索</el-button>
       </el-col>
+      
+      <!-- 预留一个位置，没有内容不占位置-->
+      <el-col :span="9"></el-col>
+
+      <!-- 右侧的下拉菜单-->
+      <el-col :span="2" :offset="9">
+        <el-dropdown trigger="click">
+          <span class="el-dropdown-link">
+            <el-button type="primary" icon="el-icon-s-fold">操作</el-button>
+          </span>
+          <el-dropdown-menu slot="dropdown">
+            <el-dropdown-item icon="el-icon-plus">添加账号</el-dropdown-item>
+            <el-dropdown-item icon="el-icon-circle-plus">禁用/启用账号</el-dropdown-item>
+            <el-dropdown-item ><svg-icon icon-class="查看" /> 查看账号</el-dropdown-item>
+            <el-dropdown-item icon="el-icon-delete">删除账号</el-dropdown-item>
+          </el-dropdown-menu>
+        </el-dropdown>
+      </el-col>
     </el-row>
+
+    <!-- 表格内容-->
     <el-row  v-loading="loading" element-loading-text="拼命加载中" element-loading-spinner="el-icon-loading" >
-        <el-table :data="userList" style="width:100%" stripe border highlight-current-row @current-change="handleCurrentChange">
-          <el-table-column prop="userID" label="账号" >
-            
-          </el-table-column>
+        <el-table :data="userList" style="width:100%" stripe border highlight-current-row @selection-change="handleCurrentChange">
+            <el-table-column type="selection" ></el-table-column>
             <el-table-column prop="userID" label="账号" ></el-table-column>
             <el-table-column prop="name" label="姓名"></el-table-column>
             <el-table-column prop="gender" label="性别"></el-table-column>
@@ -58,7 +76,7 @@ export default {
       usertype:'', //用户选择的账号类型
       loading: false, //加载数据提示
       buttonDisabled: false, //搜索按钮，默认是不禁用
-      selectRow: '', //存储选中的用户值
+      selectRow: [], //存储选中的用户值
     }
   },
 
